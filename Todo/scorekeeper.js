700e07652062f6ee54193c89a99cede7059006b4
sandun prasanganath <33305140+prasanganath@users.noreply.github.com>
2018-09-26 15:12:16 +0530

    update js

--- Todo/scorekeeper.js
+++ Todo/scorekeeper.js
@@ -1,7 +1,10 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput = document.querySelector("input");
+var p = document.querySelector("p");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
@@ -14,6 +17,7 @@ p1Button.addEventListener("click", function(){
 
 	 p1Score++;
 	 if (p1Score === winningScore) {
+	 	p1Display.classList.add("winner");
 	 	gameOver = true;
 
 	 }
@@ -28,12 +32,33 @@ p2Button.addEventListener("click", function(){
 
 	 p2Score++;
 	 if (p2Score === winningScore) {
+	 	p2Display.classList.add("winner");
 	 	gameOver = true;
 	 }
 	
 	 p2Display.textContent = p2Score;
 	} 
 });
+
+resetButton.addEventListener("click", function(){
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+
+
+
+});
+
+numInput.addEventListener("change ", function(){
+	p.textContent = "value change !"
+
+});
+
+
 
 
 
